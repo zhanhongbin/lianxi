e0cdb60fd5a9ed0bb652b62573f84f53bfaa96cd
zhanhongbin <1228103181@qq.com>
2020-06-05 21:22:16 +0800

    nihao

--- Scripts/test_search.py
+++ Scripts/test_search.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 
 from Base.base_object import Base_object
@@ -19,13 +20,21 @@ class Test_search:
     def teardown_class(self):
         Driver.quit_app_driver()
 
+
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.step("设置点击搜索")
     @pytest.fixture(scope="class",autouse=True,)
     def click_search(self):
         Base_object.base_object_set().Page_Set_click()
+        allure.attach("设置点击搜索")
 
+
+
+    @allure.step("设置搜索内容判断是否存在")
     @pytest.mark.parametrize("text,loc",data_tool("data_search.yaml"))
     def test_search(self,text,loc):
         Base_object.base_object_search().page_set_search_input(text)
+        allure.attach("设置搜索内容判断是否存在")
         assert loc in Base_object.base_object_search().page_set_search_text()
 
 
